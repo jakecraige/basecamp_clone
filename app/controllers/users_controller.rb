6e392fca392f155648c6a21aa3b268341b779865
Jake Craige <james.craige@gmail.com>
2013-06-15 15:48:29 -0500

    Sign up errors set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
-
+  def create
+    #raise params[:user].inspect
+    @user = User.new(params[:user])
+    if @user.save
+      #handle saving
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
+  end
   def index
     redirect_to "/users/new"
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,7 +7,6 @@ describe UsersController do
     before(:each) do
       @user = FactoryGirl.create(:user)
     end
-
     it "should be successful" do
       get :show, id: @user
       response.should be_success
@@ -26,6 +25,23 @@ describe UsersController do
     it "should be successful" do
       get :new
       response.should be_success
+    end
+  end
+  describe "POST create" do
+    describe "failure" do
+      before(:each) do
+        @attr = { name: "", email: "",
+                  password: "", password_confirmation: ""}
+      end
+      it "should render the new page" do
+        post :create, user: @attr
+        response.should render_template('new')
+      end
+      it "should not create a user" do
+        lambda do
+          post :create, user: @attr
+        end.should_not change(User, :count)
+      end
     end
   end
 end
