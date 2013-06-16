3717ca54d4dc37c4595f881d00bc86718aad0f3b
Jake Craige <james.craige@gmail.com>
2013-06-16 02:27:11 -0500

    handling invalid sign ins

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,13 +1,30 @@
 require 'spec_helper'
 
 describe SessionsController do
+  render_views
 
   describe "GET 'new'" do
     it "returns http success" do
       get :new
       response.should be_success
     end
+  end
+  describe "POST 'create'" do
+    describe "Failure" do
+      before(:each) do
+        @attr = { email: "",
+                  password: "" }
+      end
 
+      it "should render new page" do
+        post :create, session: @attr
+        response.should be_success
+      end
+      it "shoudl have an error message" do
+        post :create, session: @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+    end
   end
 
 end
