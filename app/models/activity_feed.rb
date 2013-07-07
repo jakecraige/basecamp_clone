95c5b3160e054137031e1d5df1d7b9cb38efa7f7
Jake Craige <james.craige@gmail.com>
2013-07-07 18:16:45 -0500

    Add tracking of comments and tasks to activity feed

--- app/models/activity_feed.rb
+++ app/models/activity_feed.rb
@@ -9,28 +9,46 @@ class ActivityFeed < ActiveRecord::Base
     info.sort_by {|k| k[:time] }.reverse.take(5)
   end
 
-  def self.format(objects, hash)
+  def self.format(objects, hash = {})
     info = []
     objects.each do |object|
+      obj_info(object, info, hash)
+    end
+    info
+  end
+  def self.action(object)
+    return 'completed' if object.class == Task && object.complete?
+    return 'updated' if object.updated_at > object.created_at
+    'added'
+  end
+  def self.obj_info(object, info, hash = {})
+
       id = object.id
       type = object.class.to_s
       time = object.updated_at
-      action = 'added'
+      action = action(object)
       creator = ''
       
       if hash[:caller] != 'User'
-        if object.class != User && object.class != List
+        if object.class != User && object.class != List && object.class != Task
           creator = object.user.name
         end
       end
 
-      if object.updated_at > object.created_at
-        action = 'updated'
+      case object.class.to_s
+        when 'Discussion'
+          comments = Comment.where(discussion_id: id).order('updated_at DESC').limit(5)
+          comments.each do |comment|
+            obj_info(comment, info)
+          end
+        when 'List'
+          tasks = Task.where(list_id: id).order('updated_at DESC').limit(5)
+          tasks.each do |task|
+            obj_info(task, info)
+          end
       end
 
       info << {id: id, type: type, creator: creator,
                time: time, action: action}
-    end
-    info
   end
 end
