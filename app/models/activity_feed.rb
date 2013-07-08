c7e0f967103eef1d04085bc7c1aab6d5730ac035
jakecraige <james.craige@gmail.com>
2013-07-08 12:42:03 -0500

    Change activityfeed methods to private and change task display format when updated

--- app/models/activity_feed.rb
+++ app/models/activity_feed.rb
@@ -9,46 +9,47 @@ class ActivityFeed < ActiveRecord::Base
     info.sort_by {|k| k[:time] }.reverse.take(5)
   end
 
-  def self.format(objects, hash = {})
-    info = []
-    objects.each do |object|
-      obj_info(object, info, hash)
+  private
+    def self.format(objects, hash = {})
+      info = []
+      objects.each do |object|
+        obj_info(object, info, hash)
+      end
+      info
     end
-    info
-  end
-  def self.action(object)
-    return 'completed' if object.class == Task && object.complete?
-    return 'updated' if object.updated_at > object.created_at
-    'added'
-  end
-  def self.obj_info(object, info, hash = {})
+    def self.action(object)
+      return 'completed' if object.class == Task && object.complete?
+      return 'updated' if object.updated_at > object.created_at
+      'added'
+    end
+    def self.obj_info(object, info, hash = {})
 
-      id = object.id
-      type = object.class.to_s
-      time = object.updated_at
-      action = action(object)
-      creator = ''
-      
-      if hash[:caller] != 'User'
-        if object.class != User && object.class != List && object.class != Task
-          creator = object.user.name
+        id = object.id
+        type = object.class.to_s
+        time = object.updated_at
+        action = action(object)
+        creator = ''
+        
+        if hash[:caller] != 'User'
+          if object.class != User && object.class != List && object.class != Task
+            creator = object.user.name
+          end
         end
-      end
 
-      case object.class.to_s
-        when 'Discussion'
-          comments = Comment.where(discussion_id: id).order('updated_at DESC').limit(5)
-          comments.each do |comment|
-            obj_info(comment, info)
-          end
-        when 'List'
-          tasks = Task.where(list_id: id).order('updated_at DESC').limit(5)
-          tasks.each do |task|
-            obj_info(task, info)
-          end
-      end
+        case object.class.to_s
+          when 'Discussion'
+            comments = Comment.where(discussion_id: id).order('updated_at DESC').limit(5)
+            comments.each do |comment|
+              obj_info(comment, info)
+            end
+          when 'List'
+            tasks = Task.where(list_id: id).order('updated_at DESC').limit(5)
+            tasks.each do |task|
+              obj_info(task, info)
+            end
+        end
 
-      info << {id: id, type: type, creator: creator,
-               time: time, action: action}
-  end
+        info << {id: id, type: type, creator: creator,
+                time: time, action: action}
+    end
 end
